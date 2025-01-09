c1ac82a39e3d60237afedca85074850b1b719b32
safaminhaj <safaminhaj11@gmail.com>
2025-01-09 22:54:44 +0530

    about and loading container

--- components/global/LoadingContainer.tsx
+++ components/global/LoadingContainer.tsx
@@ -1,5 +1,26 @@
+import { Card, CardContent } from "../ui/card";
+import { Skeleton } from "../ui/skeleton";
+
 const LoadingContainer = () => {
-  return <div>LoadingContainer</div>;
+  return (
+    <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3 ">
+      <LoadingProduct />
+      <LoadingProduct />
+      <LoadingProduct />
+    </div>
+  );
 };
+
+function LoadingProduct() {
+  return (
+    <Card>
+      <CardContent className="p-4">
+        <Skeleton className="w-full h-48" />
+        <Skeleton className="w-3/4 h-4 mt-4" />
+        <Skeleton className="w-1/2 h-4 mt-2" />
+      </CardContent>
+    </Card>
+  );
+}
 
 export default LoadingContainer;
