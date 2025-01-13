f82e0816dfb8329e9c7beae03525759fb8257327
safaminhaj <safaminhaj11@gmail.com>
2025-01-13 19:19:32 +0530

    products page

---
+++ app/products/loading.tsx
@@ -0,0 +1,13 @@
+"use client";
+import LoadingContainer from "@/components/global/LoadingContainer";
+import React from "react";
+
+const loading = () => {
+  return (
+    <div>
+      <LoadingContainer />
+    </div>
+  );
+};
+
+export default loading;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,13 @@
-const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+import ProductsContainer from "@/components/products/ProductsContainer";
+
+const ProductsPage = ({
+  searchParams,
+}: {
+  searchParams: { layout?: string; search?: string };
+}) => {
+  const layout = searchParams.layout || "grid";
+  const search = searchParams.search || "";
+  return <ProductsContainer layout={layout} search={search} />;
 };
 
 export default ProductsPage;
