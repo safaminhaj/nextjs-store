123c3492825169b8a7049d974fefafc06990b68c
safaminhaj <safaminhaj11@gmail.com>
2025-01-29 11:25:04 +0530

    loading table added

---
+++ components/global/LoadingTable.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from "../ui/skeleton";
+const LoadingTable = ({ rows = 5 }: { rows?: number }) => {
+  const tableRows = Array.from({ length: rows }, (_, i) => {
+    return (
+      <div className="mb-4" key={i}>
+        <Skeleton className="w-full h-8 rounded" />
+      </div>
+    );
+  });
+  return <>{tableRows}</>;
+};
+
+export default LoadingTable;
