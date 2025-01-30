10f6030efed12ae0832dec95766e64fbc03ebcbf
safaminhaj <safaminhaj11@gmail.com>
2025-01-30 13:26:08 +0530

    test

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -3,11 +3,11 @@ import { CardSignInButton } from "../form/Buttons";
 import FavoriteToggleForm from "./FavoriteToggleForm";
 import { fetchFavoriteId } from "@/utils/actions";
 
-const FavoriteToggleButton = async ({ productId }: { productId: string }) => {
-  const userId = auth();
+async function FavoriteToggleButton({ productId }: { productId: string }) {
+  const { userId } = auth();
   if (!userId) return <CardSignInButton />;
   const favoriteId = await fetchFavoriteId({ productId });
-  return <FavoriteToggleForm favoriteId={favoriteId} productId={productId} />;
-};
 
+  return <FavoriteToggleForm favoriteId={favoriteId} productId={productId} />;
+}
 export default FavoriteToggleButton;
